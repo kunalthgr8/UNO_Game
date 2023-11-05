ece23c56da1283ac2a096eebadbe8ab9631ebedb
kunathgr8 <kunalsingla@iitbhilai.ac.in>
2023-11-05 19:33:31 +0530

    Created BuildDeck and ShuffelDeck function

--- ShuffelDeck.py
+++ ShuffelDeck.py
@@ -1,13 +1,13 @@
 # Shuffling a list of items passed into it
 # Parameters :- Deck list
-# Returning :- Deck list
+# Returning :- Null
 
 import random 
 
 def shuffelDeck(uno_deck):
     n = len(uno_deck)
     for i in range(n - 1, 0, -1):
+        # Generate a random index between 0 and i (inclusive)
         j = random.randint(0, i)
+        # Swap the elements at indices i and j
         uno_deck[i], uno_deck[j] = uno_deck[j], uno_deck[i]
-
-    return uno_deck
